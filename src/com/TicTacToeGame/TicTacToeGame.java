26d9f60c7e521f63aaa0063e922d70f8736c9261
hritu567 <hritumog96@gmail.com>
2020-11-05 11:31:12 +0530

    Added : choosing letters

--- src/com/TicTacToeGame/TicTacToeGame.java
+++ src/com/TicTacToeGame/TicTacToeGame.java
@@ -1,19 +1,36 @@
 package com.TicTacToeGame;
-
+import java.util.Scanner;
+/*uc2*/
 public class TicTacToeGame {
-
-	public static void main(String[] args) {
-		char[] board = creategameBoard();
-
-	}
-	private static char[] creategameBoard()
+	public static Scanner sc = new Scanner(System.in);
+	public static char player, computer;
+	private void createGameBoard()
 	{
 		char[] board = new char[10];
-		for(int i=1;i<board.length ;i++)
+		for(int idx = 1; idx < board.length ; idx++)
 		{
-			board[i]= ' ';
+			board[idx]= ' ';
 		}
-		return board;
+	}
+	private void chooseLetter() {
+		System.out.println("Enter the Letter 'X' or 'O'");
+		while (true) {
+			player = sc.next().charAt(0);
+			if (player == 'X' || player == 'x') {
+				computer = 'O';
+			} else if (player == '0' || player == 'o') {
+				computer = 'X';
+			} else {
+				System.out.println("Invalid Choice, Please enter letter 'X' or 'O'");
+				continue;
+			}
+		}
 	}
 
+	public static void main(String[] args) {
+		TicTacToeGame ticTacToe = new TicTacToeGame();
+		ticTacToe.createGameBoard();
+		ticTacToe.chooseLetter();
+
+	}
 }
