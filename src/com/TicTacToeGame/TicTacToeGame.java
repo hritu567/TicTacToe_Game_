0634a73dca434e5ba03331e9343755fcf1d06735
hritu567 <68611383+hritu567@users.noreply.github.com>
2020-11-05 11:02:05 +0530

    Update TicTacToeGame.java

--- src/com/TicTacToeGame/TicTacToeGame.java
+++ src/com/TicTacToeGame/TicTacToeGame.java
@@ -3,10 +3,10 @@ package com.TicTacToeGame;
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
-		char[] board = creategameBoard();
+		char[] board = createGameBoard();
 
 	}
-	private static char[] creategameBoard()
+	private static char[] createGameBoard()
 	{
 		char[] board = new char[10];
 		for(int i=1;i<board.length ;i++)
