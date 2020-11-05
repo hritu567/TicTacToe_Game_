ad0b71e3b4052adb91ec5fc6842e4d5cdf45041f
hritu567 <hritumog96@gmail.com>
2020-11-05 13:18:15 +0530

    Added: Priniting out the board

--- src/com/TicTacToeGame/TicTacToeGame.java
+++ src/com/TicTacToeGame/TicTacToeGame.java
@@ -1,21 +1,23 @@
 package com.TicTacToeGame;
 import java.util.Scanner;
-/*uc2*/
+/*uc3*/
 public class TicTacToeGame {
 	public static Scanner s = new Scanner(System.in);
 	public static char player, computer;
-	private void createGameBoard()
+	
+	private char[] createGameBoard()
 	{
 		char[] board = new char[10];
 		for(int idx = 1; idx < board.length ; idx++)
 		{
 			board[idx]= ' ';
 		}
-	}
-	/* choosing letter for player and computer */
-	private void chooseLetter() {
+		return board;
+	}	
+	/* choosing letter for player and computer */	
+	/*private void chooseLetter() {
 		System.out.println("Enter the Letter 'X' or 'O'");
-		while (true) {
+		//while (true) {
 			player = s.next().charAt(0);
 			if (player == 'X' || player == 'x') {
 				computer = 'O';
@@ -23,15 +25,25 @@ public class TicTacToeGame {
 				computer = 'X';
 			} else {
 				System.out.println("Invalid Choice, Please enter letter 'X' or 'O'");
-				continue;
+				player = s.next().charAt(0);
 			}
 		}
-	}
+		*/
+	//}
+	/* Printing out the game Board */
+	private void showBoard(char[] board) {
+
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("--+---+--");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("--+---+--");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+		}
 /* calling other functions into main function*/
 	public static void main(String[] args) {
 		TicTacToeGame ticTacToe = new TicTacToeGame();
-		ticTacToe.createGameBoard();
-		ticTacToe.chooseLetter();
-
+		char[] board = ticTacToe.createGameBoard();
+		//ticTacToe.chooseLetter();
+		ticTacToe.showBoard(board);
 	}
 }
